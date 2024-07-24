f05ecef26353abca5e97353416a24a34d0d38bad
Noah Stapp <noah.stapp@mongodb.com>
2024-07-24 09:53:09 -0700

    socket test

--- windows_test.py
+++ windows_test.py
@@ -1,28 +1,24 @@
 import asyncio
 import threading
-from pymongo import AsyncMongoClient
 import socket
 
-clients = []
+socks = []
 
 
 async def create_socket():
-    c = socket.socket()
-    c.connect(("www.python.org", 80))
-    c.settimeout(0.0)
-    loop = asyncio.get_event_loop()
-    print(f"{threading.current_thread().name}: {await asyncio.wait_for(loop.sock_sendall(c, bytes('hello', 'utf-8')), timeout=5)}")
+    s = socket.socket()
+    s.connect(("www.python.org", 80))
+    s.settimeout(0.0)
+    print(f"{threading.current_thread().name}: {s.sendall(bytes('hello', 'utf-8'))}")
 
-    clients.append(c)
+    socks.append(s)
 
 
 async def use_socket():
-    while len(clients) < 1:
+    while len(socks) < 1:
         pass
-    c = clients.pop()
-    c.settimeout(0.0)
-    loop = asyncio.get_event_loop()
-    print(f"{threading.current_thread().name}: {await asyncio.wait_for(loop.sock_sendall(c, bytes('hello', 'utf-8')), timeout=5)}")
+    s = socks.pop()
+    print(f"{threading.current_thread().name}: {s.sendall(bytes('hello', 'utf-8'))}")
 
 
 def wrapper(func):
