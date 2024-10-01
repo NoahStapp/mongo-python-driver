99ae6e33eda2e17a1cf305cc7bac4a1af00c13f3
Noah Stapp <noah.stapp@mongodb.com>
2024-10-01 16:39:43 -0400

    debug

--- pymongo/network_layer.py
+++ pymongo/network_layer.py
@@ -235,7 +235,7 @@ async def async_receive_data(
         for task in pending:
             task.cancel()
         while not all([t.done() for t in pending]):  # noqa: C419
-            loop._run_once()
+            await asyncio.sleep(0)
         if len(done) == 0:
             raise socket.timeout("timed out")
         if read_task in done:
