c96eba719a9acce31f46ea7f4929dabd95323c74
Noah Stapp <noah.stapp@mongodb.com>
2025-02-25 12:30:22 -0500

    more debugging

--- pymongo/network_layer.py
+++ pymongo/network_layer.py
@@ -756,7 +756,7 @@ async def async_receive_message(
         # timeouts on AWS Lambda and other FaaS environments.
         timeout = max(deadline - time.monotonic(), 0)
     if asyncio.current_task() and "monitor" not in asyncio.current_task().get_name() and "rtt" not in asyncio.current_task().get_name():
-        print(f"in {asyncio.current_task().get_name()}, calling async_receive_message from {''.join(traceback.format_stack(limit=15))}")
+        print(f"in {asyncio.current_task().get_name()}, calling async_receive_message from {''.join(traceback.format_stack(limit=30))}")
     cancellation_task = create_task(_poll_cancellation(conn))
     read_task = create_task(conn.conn.get_conn.read(request_id, max_message_size, debug))
     tasks = [read_task, cancellation_task]
